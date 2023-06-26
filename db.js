34d7de706fd9498934f004898ee2e304b1edf0c6
nolood <a9514548799@gmail.com>
2023-06-26 19:19:54 +0500

    updated password

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ const { Sequelize } = require("sequelize");
 module.exports = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  // process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD,
   {
     dialect: "postgres",
     host: process.env.DB_HOST,
