71003c7b403624a4a70fb355b6a2f7d6ad284d09
nolood <a9514548799@gmail.com>
2023-07-03 19:11:07 +0500

    test basket

--- controllers/BasketController.js
+++ controllers/BasketController.js
@@ -2,54 +2,11 @@ const { Basket, User } = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class BasketController {
-  async createBasket(userId, req, res) {
+  async sendEmail(req, res) {
     try {
-      const user = await User.findByPk(userId);
-      if (!user) {
-        return res.status(404).json({ message: "invalid userId" });
-      }
-      const basket = await Basket.create({ userId });
-      return res.json(true);
+      res.json(req.body);
     } catch (e) {
       res.status(404).json(e.message);
-    }
-  }
-
-  async getBasketByUserId(req, res, next) {
-    try {
-      const { userId } = req.params;
-      const basket = await Basket.findOne({ where: { userId } });
-      return res.json(basket);
-    } catch (e) {
-      next(ApiError.badRequest(e.message));
-    }
-  }
-
-  async updateBasketByUserId(req, res, next) {
-    try {
-      const { userId } = req.params;
-      const {
-        /* Здесь указывайте поля, которые нужно обновить */
-      } = req.body;
-      const updatedBasket = await Basket.update(
-        {
-          /* Здесь указывайте поля и их значения для обновления */
-        },
-        { where: { userId } }
-      );
-      return res.json(updatedBasket);
-    } catch (e) {
-      next(ApiError.badRequest(e.message));
-    }
-  }
-
-  async deleteBasketByUserId(req, res, next) {
-    try {
-      const { userId } = req.params;
-      await Basket.destroy({ where: { userId } });
-      return res.json({ message: "Basket deleted successfully" });
-    } catch (e) {
-      next(ApiError.badRequest(e.message));
     }
   }
 }
--- routes/basketRouter.js
+++ routes/basketRouter.js
@@ -2,9 +2,6 @@ const Router = require("express");
 const router = new Router();
 const basketController = require("../controllers/BasketController");
 
-router.post("/", basketController.createBasket);
-router.get("/:userId", basketController.getBasketByUserId);
-router.put("/:userId", basketController.updateBasketByUserId);
-router.delete("/:userId", basketController.deleteBasketByUserId);
+router.post("/", basketController.sendEmail);
 
 module.exports = router;
