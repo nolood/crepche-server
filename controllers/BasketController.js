1bc49ccd7fd81e0ef9956dd19d934808118fb444
nolood <a9514548799@gmail.com>
2023-07-08 13:40:44 +0500

    Update BasketController.js

--- controllers/BasketController.js
+++ controllers/BasketController.js
@@ -1,9 +1,20 @@
 const { Offer } = require("../models/models");
 
 class BasketController {
-  async sendEmail(req, res) {
+  async sendItems(req, res) {
     try {
-      return res.json(req.body);
+      const { name, surname, phone, items } = req.body;
+      const item = await Offer.create({ name, surname, number: phone, items });
+      return res.status(200).json(item);
+    } catch (e) {
+      return res.status(404).json(e.message);
+    }
+  }
+
+  async getItems(req, res) {
+    try {
+      const items = Offer.findAll({ order: [["createdAt", "asc"]] });
+      return res.status(200).json(items);
     } catch (e) {
       return res.status(404).json(e.message);
     }
